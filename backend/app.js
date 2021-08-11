a28e6534f7cc3fd7144924cff2eb8c82cfa027b8
Chantalak <76796041+Chantalak@users.noreply.github.com>
2021-08-11 16:09:38 +0200

    add express

---
+++ backend/app.js
@@ -0,0 +1,10 @@
+//fichier qui contient notre application 
+//importation d'express
+const express = require('express');
+
+//application express
+const app = express();
+
+
+//exportation application
+module.exports = app;
---
+++ backend/server.js
@@ -0,0 +1,54 @@
+//contient code server node
+//création d'un programme qui écoute et attend requete HTTP
+//commence par importer package http de node 
+const http = require('http');
+//importation app.js
+const app = require('./app');
+
+//appelle méthode createServer du package http
+//prend comme argument fonction appelée à requete reçu par server
+const normalizePort = val => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const server = http.createServer(app);
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+//pour écouter et attendre requete envoyée
+server.listen(process.env.PORT || 3000);
